bc6fbbb589a8ad9a348526da050f6dfd42996dae
yuan3675 <yuan3675@gmail.com>
2017-09-15 06:51:35 -0700

    Q1 half finish

---
+++ hw0/Q1.py
@@ -0,0 +1,26 @@
+inputFile = open('words.txt', 'r')
+outputFile = open('Q1.txt', 'w')
+s = inputFile.read()
+s = s.split()
+index = 0
+result = []
+for i in s:
+	find = 0
+	for j in result:
+		if j[0] == i:
+			j[1] = j[1] + 1
+			find = 1
+	if find != 1:
+		result.append([i,1])
+
+for i in result:
+	outputFile.write(i[0])
+	outputFile.write(' ')
+	outputFile.write(str(index))
+	outputFile.write(' ')
+	outputFile.write(str(i[1]))
+	outputFile.write('\n')
+	index = index + 1
+
+inputFile.close()
+outputFile.close()
