correct gradient descent, score 6.89

hw1/train.py
@@ -1,22 +1,25 @@
 import sys
 import csv
+import math
 import pandas as pd
 import numpy as np
 import CSVreader
 import dataProcessing
 
 #define parameters
-iteration = 10000
-learningRate = 0.000001
-parameter = np.array([[0.1], [0.1], [0.1], [0.1], [0.1], [0.1], [0.1], [0.1], [0.1], [0.1]])
+iteration = 500000
+learningRate = 0.0002
+parameter = np.array([[0.01], [0.01], [0.01], [0.01], [0.01], [0.01], [0.01], [0.01], [0.01]
+                      , [0.01], [0.01], [0.01], [0.01], [0.01], [0.01], [0.01], [0.01], [0.01], [0.01]])
 
 def hypoFunction(parameters, data):
+    #print(data.shape, parameters.shape)
     predictValue = np.dot(data, parameters)
     return predictValue
 
-def costFunction(predictValue, actualValue):
+def errorRateFunction(predictValue, actualValue):
     error = (predictValue - actualValue)**2
-    return np.sum(error)
+    return math.sqrt(np.sum(error))
 
 
 #get data
@@ -24,7 +27,8 @@ reader = CSVreader.CSVreader()
 process = dataProcessing.dataProcessing()
 data = pd.DataFrame(reader.readTrain(sys.argv[1]))
 test = pd.DataFrame(reader.readTest(sys.argv[2]))
-"""
+
+
 trainData = process.getTrain(data)
 #validData = process.getValid(data)
 testData = process.getTest(test)
@@ -42,20 +46,22 @@ n = 10
 
 for i in range(iteration):
     predictValue = hypoFunction(parameter, trainSet)
-    #errorRate = (costFunction(predictValue, trainTargetSet))/train_m * 50
-    parameter = parameter - learningRate * np.dot(np.transpose(trainSet), (predictValue - trainTargetSet))
-    if(i % 500 == 0):
-        errorRate = (costFunction(predictValue, trainTargetSet))/train_m * 50
-        print(i,'training error rate:', errorRate, '%')
+    parameter = parameter - learningRate * np.dot(np.transpose(trainSet), (predictValue - trainTargetSet)) / train_m
+    if i % 1000 == 0:
+        errorRate = errorRateFunction(predictValue, trainTargetSet)
+        print(i,'training error:', errorRate)
+        
 
 
 
 #compute validation error rate
-predictValue = hypoFunction(parameter, validSet)
-errorRate = (costFunction(predictValue, validTargetSet))/valid_m * 100
-print('Validation error rate=', errorRate, '%')
+#predictValue = hypoFunction(parameter, validSet)
+#errorRate = (costFunction(predictValue, validTargetSet))/valid_m * 100
+#print('Validation error rate=', errorRate, '%')
 
 #print(parameter)
+#print(predictValue, trainTargetSet)
+
 
 #compute test
 predictValue = hypoFunction(parameter, testData)
@@ -69,4 +75,5 @@ with open('testOutput.csv', 'w', newline='') as csvfile:
         writer.writerow(['id_'+str(index), i[0]])
         index = index + 1
 
-"""
+
+