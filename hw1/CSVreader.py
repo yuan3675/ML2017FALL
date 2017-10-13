b0f7361dd1834721521c5376423c01166380b2ef
yuan3675 <r06922152@ntu.edu.tw>
2017-10-13 04:50:01 -0700

    revise CSVreader

--- hw1/CSVreader.py
+++ hw1/CSVreader.py
@@ -14,9 +14,9 @@ class CSVreader:
             for row in reader:
                 del row[0]
                 del row[0]
-                #if row[0] == 'PM2.5':
-                del row[0]
-                self.trainList.append(row)
+                if row[0] == 'PM2.5':
+                	del row[0]
+                	self.trainList.append(row)
             return(self.trainList)
         
     def readTest(self, filename):
@@ -24,9 +24,9 @@ class CSVreader:
             reader = csv.reader(csvfile, delimiter=',')
             for row in reader:
                 del row[0]
-                #if row[0] == 'PM2.5':
-                del row[0]
-                self.testList.append(row)
+                if row[0] == 'PM2.5':
+                	del row[0]
+                	self.testList.append(row)
             return(self.testList)
 
     def readParameters(self, filename):
